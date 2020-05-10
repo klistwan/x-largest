3761d48fcfb700530bb12289342ad5ba67517bb3
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2020-05-10 18:32:07 +0900

    Add argparse to handle command line arguments.

---
+++ main.py
@@ -0,0 +1,53 @@
+import argparse
+import heapq
+import sys
+
+class Record:
+	def __init__(self, file_line):
+		uid, value = file_line.strip().split()
+		self.uid = uid
+		self.value = int(value)
+
+	def __lt__(self, other):
+	    return self.value < other.value
+
+	def __str__(self):
+	    return self.uid
+
+
+class TopRecordHandler:
+	def __init__(self, x: int):
+		self.heap = []
+		self.max_elements = x
+
+	def add(self, record: Record) -> None:
+		if len(self.heap) < self.max_elements:
+			self.heap.append(record)
+			if len(self.heap) == self.max_elements:
+				heapq.heapify(self.heap)
+		else:
+			if self.heap[0] < record:
+				heapq.heapreplace(self.heap, record)
+
+	def output(self):
+		print("\n".join([r.uid for r in self.heap]))
+
+
+def main():
+	parser = argparse.ArgumentParser(description='Report x largest values.')
+	parser.add_argument('x_largest', type=int, help='number of largest values')
+	parser.add_argument('--file', '-f', nargs='?')
+	_args = parser.parse_args()
+
+	# Open from file if supplied, else from stdin.
+	lines = open(_args.file) if _args.file else sys.stdin
+
+	handler = TopRecordHandler(_args.x_largest)
+	for line in lines:
+		record = Record(line)
+		handler.add(record)
+	handler.output()
+
+
+if __name__ == '__main__':
+	main()
